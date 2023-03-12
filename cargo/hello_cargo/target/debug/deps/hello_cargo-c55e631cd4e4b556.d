1733105074447b967f58f8aa187ee5aae6d819d4
alicorn0618 <alicorn0618@outlook.com>
2023-03-12 23:17:02 +0800

    "inital"

---
+++ cargo/hello_cargo/target/debug/deps/hello_cargo-c55e631cd4e4b556.d
@@ -0,0 +1,5 @@
+/home/nerd/rust_learning/cargo/hello_cargo/target/debug/deps/hello_cargo-c55e631cd4e4b556: src/main.rs
+
+/home/nerd/rust_learning/cargo/hello_cargo/target/debug/deps/hello_cargo-c55e631cd4e4b556.d: src/main.rs
+
+src/main.rs:
---
+++ cargo/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/nerd/rust_learning/cargo/hello_cargo/target/debug/hello_cargo: /home/nerd/rust_learning/cargo/hello_cargo/src/main.rs
